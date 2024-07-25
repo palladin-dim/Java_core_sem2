20e9c363740db31224c1870b87ced54c29a842b8
Dmityi <palladin-dim@mail.ru>
2024-07-25 18:31:04 +1100

    Java-core_sem2_1

---
+++ src/shum/FindDifference.java
@@ -0,0 +1,31 @@
+package ru.shum;
+
+public class FindDifference {
+    /**
+     * Написать функцию, возвращающую разницу между самым большим и самым ма- леньким элементами
+     * переданного не пустого массива.
+     *
+     * @param nums Принимает массив
+     * @return Возвращаем разницу между максимальным и минимальным элементами
+     */
+    public static int findDifference(int[] nums) {
+        if (nums.length <= 1) {
+            return 0; // или другое значение по умолчанию
+        }
+
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        return max - min;
+    }
+
+}
